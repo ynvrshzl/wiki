923e6a78d6050988dbd8ef045d0032d980451b1f
Hazl <ynvrshzl@gmail.com>
2026-05-26 03:26:20 -0400

    Add preprocessor script for wiki filesystem formatting

---
+++ cli/preprocessor.cjs
@@ -0,0 +1,6 @@
+// This script will recreate the local wiki filesystem to ensure files are formatted correctly.
+// Reference documentation [...] for formatting guidelines.
+
+// Program overview
+// 1. Find empty links
+// 2. Find broken links
