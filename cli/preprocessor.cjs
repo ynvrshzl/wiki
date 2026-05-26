106921ca1b0f845069cdb619870e4aad439bf939
Hazl <ynvrshzl@gmail.com>
2026-05-26 03:36:11 -0400

    Update comments for clarity in preprocessor.cjs

--- cli/preprocessor.cjs
+++ cli/preprocessor.cjs
@@ -1,6 +1,7 @@
+// To assist in the structure of content and information distribution in this project,
 // This script will recreate the local wiki filesystem to ensure files are formatted correctly.
 // Reference documentation [...] for formatting guidelines.
 
 // Program overview
-// 1. Find empty links
-// 2. Find broken links
+// * Find empty links (these links are temporary placeholders.)
+// * Find broken links (and eventually attempt to route correct ancestor in fs, or notify in CLI.)
